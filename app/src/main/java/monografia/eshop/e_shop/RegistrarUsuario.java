d45ab1dfb241c8bbd0412bb3f6f99f229842678b
Ruben <rdariomartinez@ucundinamarca.edu.co>
2023-10-17 20:49:25 -0500

    bd

--- app/src/main/java/monografia/eshop/e_shop/RegistrarUsuario.java
+++ app/src/main/java/monografia/eshop/e_shop/RegistrarUsuario.java
@@ -4,8 +4,14 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +20,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,10 +31,25 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-public class RegistrarUsuario extends AppCompatActivity implements Response.Listener<JSONObject>,Response.ErrorListener{
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 
+public class RegistrarUsuario extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+
+    private static final String CARPETA_PRINCIPAL = "misImagenesApp/";
+    private static final String CARPETA_IMAGEN = "Imagenes";
+    private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;
+    private String path;
+    File fileImagen;
+    Bitmap bitmap;
+
+    private static final int COD_SELECCIONA = 10;
+    private static final int COD_FOTO = 20;
     EditText camp1, camp2, camp3, camp4, camp5, camp6, camp7, camp8, camp9, camp10, camp11, camp12;
-    Button botRegUsu, botFot;
+    Button btnTerRegUsu, btnFot;
     ProgressDialog progreso;
     ImageView imgFoto;
 
@@ -51,74 +73,130 @@ public class RegistrarUsuario extends AppCompatActivity implements Response.List
         camp10 = (EditText) findViewById(R.id.txtDir_Usu);
         camp11 = (EditText) findViewById(R.id.txtUsu_Usu);
         camp12 = (EditText) findViewById(R.id.txtCon_Usu);
-        botRegUsu = (Button) findViewById(R.id.btnTerRegUsu);
-        imgFoto = (ImageView) findViewById(R.id.imagenId);
+        btnTerRegUsu = (Button) findViewById(R.id.btnTerRegUsu);
+        btnFot = (Button) findViewById(R.id.btnFot);
+        imgFoto = (ImageView) findViewById(R.id.imgFoto);
 
-        request = Volley.newRequestQueue(this);
+        request = Volley.newRequestQueue(getApplicationContext());
 
-        botRegUsu.setOnClickListener(new View.OnClickListener() {
+        btnTerRegUsu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void  onClick (View view) {
+            public void onClick(View view) {
                 insertarDatosNuevoUsuario();
             }
         });
 
-        /*botFot.setOnClickListener(new View.OnClickListener() {
+        btnFot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mostrarDialogOpciones();
-            }
-        });*/
-    }
-
-    private void mostrarDialogOpciones() {
-        final CharSequence[] opciones={"Tomar foto","Elegir de Galeria","Cancelar"};
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Elige una opción");
-        builder.setItems(opciones, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (opciones[i].equals("Tomar foto")) {
-                    //metodo para llamar a camara
-                    //Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_LONG).show();
-                    Toast.makeText(getContext(), "Los permisos no fueron aceptados", Toast.LENGTH_LONG).show();
-                } else {
-                    if (opciones[i].equals("Elegir de Galeria")) {
-                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        intent.setType("image/");
-                        startActivityForResult(intent.createChooser(intent, "Seleccione"),10);
-                    } else {
-                        dialogInterface.dismiss();
-                    }
-                }
             }
         });
     }
 
     public void insertarDatosNuevoUsuario() {
 
-        progreso =  new ProgressDialog(this);
+        progreso = new ProgressDialog(this);
         progreso.setMessage("Cargando...");
         progreso.show();
 
-        String url = "http://192.168.1.8/conexionEShop/RegistrarUsuario.php?cedu="+camp3.getText().toString()+
-                "&nomb="+camp1.getText().toString()+"&apel="+camp2.getText().toString()+
-                "&fnac="+camp4.getText().toString()+"&ncel="+camp5.getText().toString()+
-                "&usua="+camp11.getText().toString()+"&emai="+camp6.getText().toString()+
-                "&cont="+camp12.getText().toString()+"&dire="+camp10.getText().toString()+
-                "&barr="+camp7.getText().toString()+"&ciud="+camp8.getText().toString()+
-                "&depa="+camp9.getText().toString();
+        String url = "http://192.168.1.8/conexionEShop/RegistrarUsuario.php?" +
+                "cedu=" + camp3.getText().toString() + "&nomb=" + camp1.getText().toString() +
+                "&apel=" + camp2.getText().toString() + "&ncel=" + camp5.getText().toString() +
+                "&fnac=" + camp4.getText().toString() + "&emai=" + camp6.getText().toString() +
+                "&dire=" + camp10.getText().toString() + "&barr=" + camp7.getText().toString() +
+                "&ciud=" + camp8.getText().toString() + "&depa=" + camp9.getText().toString() +
+                "&usua=" + camp11.getText().toString() + "&cont=" + camp12.getText().toString();
 
-        url = url.replace(" ","%20");
+        url = url.replace(" ", "%20");
 
-        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null, this, this);
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
         request.add(jsonObjectRequest);
     }
 
-    public void onResponse(JSONObject response) {
-        Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_LONG).show();
-        progreso.hide();
+    private void mostrarDialogOpciones() {
+        final CharSequence[] opciones = {"Tomar foto", "Elegir de Galeria", "Cancelar"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(RegistrarUsuario.this);
+        builder.setTitle("Elige una opción");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("Tomar foto")) {
+                    abrirCamara();
+                } else {
+                    if (opciones[i].equals("Elegir de Galeria")) {
+                        try {
+                            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            intent.setType("image/");
+                            startActivityForResult(intent.createChooser(intent, "Seleccione"), COD_SELECCIONA);
+                        } catch (Exception e) {
+                        }
+                    } else {
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        builder.show();
+    }
 
+    private void abrirCamara() {
+        File miFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), DIRECTORIO_IMAGEN);
+        boolean isCreada = miFile.exists();
+        if (!isCreada) {
+            isCreada = miFile.mkdirs();
+        }
+        if (isCreada) {
+            Long consecutivo = System.currentTimeMillis() / 1000;
+            String nombre = consecutivo.toString() + ".jpg";
+
+            path = miFile.getAbsolutePath() + File.separator + nombre;
+            fileImagen = new File(path);
+
+            Uri fileUri = FileProvider.getUriForFile(this, "fileProvider", fileImagen);
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+            startActivityForResult(intent, COD_FOTO);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case COD_SELECCIONA:
+                try {
+                    if (resultCode == AppCompatActivity.RESULT_OK) {
+                        Uri miPath = data.getData();
+                        imgFoto.setImageURI(miPath);
+                    } else {
+                        // El usuario canceló la selección o no se realizó con éxito.
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Error al procesar la imagen seleccionada", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case COD_FOTO:
+                MediaScannerConnection.scanFile(RegistrarUsuario.this, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(String s, Uri uri) {
+                        Log.i("Path", "" + path);
+                    }
+                });
+                bitmap = BitmapFactory.decodeFile(path);
+                imgFoto.setImageBitmap(bitmap);
+                break;
+        }
+    }
+
+    public void onResponse(JSONObject response) {
+        Toast.makeText(RegistrarUsuario.this, "Usuario registrado correctamente", Toast.LENGTH_LONG).show();
+        progreso.hide();
+        /*
         camp1.setText("");
         camp2.setText("");
         camp3.setText("");
@@ -131,89 +209,14 @@ public class RegistrarUsuario extends AppCompatActivity implements Response.List
         camp10.setText("");
         camp11.setText("");
         camp12.setText("");
+        */
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
         progreso.hide();
-        Toast.makeText(this, "No se pudo registrar " + error.toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "No se pudo registrar " + error.toString(), Toast.LENGTH_LONG).show();
         Log.i("Error", error.toString());
-    }
-
-    public boolean validar() {
-        boolean retorno = true;
-
-        String nomb;
-        String apel;
-        String cedu;
-        String naci;
-        String celu;
-        String emai;
-        String barr;
-        String ciud;
-        String depa;
-        String dire;
-        String usua;
-        String cont;
-
-        nomb = camp1.getText().toString();
-        apel = camp2.getText().toString();
-        cedu = camp3.getText().toString();
-        naci = camp4.getText().toString();
-        celu = camp5.getText().toString();
-        emai = camp6.getText().toString();
-        barr = camp7.getText().toString();
-        ciud = camp8.getText().toString();
-        depa = camp9.getText().toString();
-        dire = camp10.getText().toString();
-        usua = camp11.getText().toString();
-        cont = camp12.getText().toString();
-
-        if (nomb.isEmpty()) {
-            camp1.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (apel.isEmpty()) {
-            camp2.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (cedu.isEmpty()) {
-            camp3.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (naci.isEmpty()) {
-            camp4.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (celu.isEmpty()) {
-            camp5.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (emai.isEmpty()) {
-            camp6.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (barr.isEmpty()) {
-            camp7.setError("Este campo no puede quedar vacio");
-        } if (ciud.isEmpty()) {
-            camp8.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (depa.isEmpty()) {
-            camp9.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (dire.isEmpty()) {
-            camp10.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (usua.isEmpty()) {
-            camp11.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        } if (cont.isEmpty()) {
-            camp12.setError("Este campo no puede quedar vacio");
-            retorno = false;
-        }
-        return retorno;
-    }
-
-    public void RegUsu (View view) {
-        if (validar()) {
-            Toast.makeText(this, "Datos agregados", Toast.LENGTH_SHORT).show();
-            Intent Cambiar = new Intent(this, IniciarSesion.class);
-            startActivity(Cambiar);
-        }
     }
 
 }
